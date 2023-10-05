5fd3c9802d96dcba6852ab37a90309822761c908
Katie Hughes <katiereeshughes@gmail.com>
2023-10-04 22:26:42 -0500

    Successfully split image into 2

--- src/split_image.cpp
+++ src/split_image.cpp
@@ -7,8 +7,6 @@
 #include <opencv2/highgui.hpp>
 #include "image_transport/image_transport.hpp"
 
-void show_image(const std::string & window, const sensor_msgs::msg::Image::ConstSharedPtr& img);
-
 class ImgSplitter : public rclcpp::Node
 {
 public:
@@ -16,14 +14,19 @@ public:
   : Node("img_splitter")
   {
     //Subscribers
-    sub_raw_ = std::make_shared<image_transport::CameraSubscriber>(
-        image_transport::create_camera_subscription(
-          this,
-          "image_raw",
-          std::bind(&ImgSplitter::raw_callback, this, std::placeholders::_1, std::placeholders::_2),
-          "compressed",
-          rclcpp::QoS {10}.get_rmw_qos_profile()
-        )
+    // sub_raw_ = std::make_shared<image_transport::CameraSubscriber>(
+    //     image_transport::create_camera_subscription(
+    //       this,
+    //       "image_raw",
+    //       std::bind(&ImgSplitter::raw_callback, this, std::placeholders::_1, std::placeholders::_2),
+    //       "compressed",
+    //       rclcpp::QoS {10}.get_rmw_qos_profile()
+    //     )
+    // );
+    sub_raw_ = create_subscription<sensor_msgs::msg::Image>(
+      "/head/front/cam/image_rect/right",
+      10,
+      std::bind(&ImgSplitter::raw_callback, this, std::placeholders::_1)
     );
 
     RCLCPP_INFO_STREAM(get_logger(), "img_subscriber node started");
@@ -37,22 +40,33 @@ public:
   }
 
 private:
-  std::shared_ptr<image_transport::CameraSubscriber> sub_raw_;
+  // std::shared_ptr<image_transport::CameraSubscriber> sub_raw_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_raw_;
 
   void raw_callback(
-    const sensor_msgs::msg::Image::ConstSharedPtr& img,
-    const sensor_msgs::msg::CameraInfo::ConstSharedPtr&
+    const sensor_msgs::msg::Image::ConstSharedPtr& img
+    // const sensor_msgs::msg::CameraInfo::ConstSharedPtr&
   ) {
-    show_image("raw img", img);
+    RCLCPP_INFO_STREAM(get_logger(), "received image");
+    const auto cv_img = cv_bridge::toCvCopy(*img, img->encoding)->image;
+    const auto w = cv_img.size().width;
+    const auto h = cv_img.size().height;
+    // corner, corner, width, height
+    const auto left = cv_img(cv::Rect(0, 0, 0.5*w, h)).clone();
+    const auto right = cv_img(cv::Rect(0.5*w, 0, 0.5*w, h)).clone();
+    const std::string left_name = "left";
+    const std::string right_name = "right";
+    cv::namedWindow(right_name);
+    cv::imshow(right_name, right);
+    cv::namedWindow(left_name);
+    cv::imshow(left_name, left);
+    cv::waitKey(1);
   }
+
 
 };
 
-void show_image(const std::string & window, const sensor_msgs::msg::Image::ConstSharedPtr& img) {
-  cv::namedWindow(window);
-  cv::imshow(window, cv_bridge::toCvCopy(*img, img->encoding)->image);
-  cv::waitKey(1);
-}
+
 
 int main(int argc, char** argv)
 {
